93890b61d355f9f380d4ee75a0eaced5081ae7da
Dhanavarshini <dp9387@srmist.edu.in>
2026-03-23 11:12:50 +0530

    [dhana]: UC1: Setup Address Book Spring Project with basic REST controller

---
+++ src/main/java/com/bridgelabz/addressspringappbook/AddressspringappbookApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressspringappbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressspringappbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressspringappbookApplication.class, args);
+	}
+
+}
